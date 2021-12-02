1bccb829e89d081909060c054f4e85403a7237f9
Yoogesh sharma <yoogesh1983@gmail.com>
2021-12-02 04:38:59 -0500

    hk

--- MFE/store/Provider.js
+++ MFE/store/Provider.js
@@ -13,10 +13,14 @@ class Provider extends Component {
         }
     }
     
+    
+     //If you dont wrap the {this.props.children} by ContextDB.Provider, it will always take the value from the default context
+    // Since you are wrapping the {this.props.children} with Provider, the context default value will be overridden by the value you provided
+    // Also notice that you can add more value if you want directly here also like we have added a new field firstName with the value 'Yoogesh'
     render(){
         return(
             <ContextDB.Provider value = {{...this.state, firstName: 'Yoogesh'}}>
-                <Home />
+                {this.props.children}
             </ContextDB.Provider>
         );
     }
